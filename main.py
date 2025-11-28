44b9bf650e37da91e1355ede3bd22dd2562a2965
sdnnh8rc97-a11y <sdnnh8rc97@privaterelay.appleid.com>
2025-11-29 02:23:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,12 @@
-from fastapi import FastAPI, File, UploadFile
-from face_engine import FaceEngine
+from flask import Flask
 
-app = FastAPI(
-    title="Face Recognition API",
-    description="InsightFace + SVM + KNN",
-    version="1.0.0"
-)
+app = Flask(__name__)
 
-engine = FaceEngine()
+@app.route("/")
+def home():
+    return "Face API is running!"
 
-@app.post("/predict")
-async def predict(file: UploadFile = File(...)):
-    img_bytes = await file.read()
-    img = engine.read_image(img_bytes)
-
-    emb = engine.get_embedding(img)
-    if emb is None:
-        return {"error": "No face detected"}
-
-    result = engine.classify(emb)
-    return result
-
-@app.get("/")
-def root():
-    return {"msg": "Face API running!"}
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
